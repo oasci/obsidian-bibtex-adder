94083b3b2165e63f34b1e8d41d36a69efd5d2bc5
Alex Maldonado <aalexmmaldonado@gmail.com>
2022-08-01 22:52:58 -0400

    fix: accented are cleaned for bibtex key

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,12 @@ export function addStr(str: string, index: number, str_add: string): string{
   return str.substring(0, index) + str_add + str.substring(index, str.length);
 }
 
+/**
+ * Retrieves first word of a string.
+ * @param str String containing one or more words. Should not contain any 
+ * accentuated characters.
+ * @returns First word of str
+ */
 export function getFirstWord(str: string): string {
   // Remove all non-alphanumeric characters
   str = str.replace(/[\W_]+/g," ");
@@ -23,17 +29,22 @@ export function getFirstWord(str: string): string {
   return first_word;
 }
 
+/**
+ * Remove accented characters in a string.
+ * @param str Any string.
+ * @returns String with all accented characters replaced with nonaccented ones.
+ */
 export function normString(str: string): string {
   return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
 
 export function gscholarBibtexKey(crossrefData: any): string {
-  let author_last_name = getFirstWord(
-    crossrefData.author[0]['family'].toLowerCase()
-  );
+  let author_last_name = crossrefData.author[0]['family'].toLowerCase();
+  author_last_name = normString(author_last_name);
+  author_last_name = getFirstWord(author_last_name);
   let year = crossrefData.published['date-parts'][0][0];
   let title = crossrefData.title[0].toLowerCase();
   let title_first_word = getFirstWord(title);
   let bibtex_key = author_last_name + year + title_first_word;
-  return normString(bibtex_key);;
+  return normString(bibtex_key);
 }
